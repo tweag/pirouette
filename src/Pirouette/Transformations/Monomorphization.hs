fa5e8f43a1b229a2734c701dac93f2af3f59e88a
0xd34df00d <0xd34df00d@gmail.com>
2022-02-28 15:05:55 -0600

    Move some things to the util module and generalize

--- src/Pirouette/Transformations/Monomorphization.hs
+++ src/Pirouette/Transformations/Monomorphization.hs
@@ -23,9 +23,6 @@ import qualified Data.Set as S
 import Data.String
 import qualified Data.Text as T
 import Data.Void
-import Prettyprinter hiding (Pretty, pretty)
-
-import Debug.Trace
 
 import Pirouette.Monad
 import Pirouette.Term.Syntax
@@ -33,6 +30,8 @@ import Pirouette.Term.Syntax.Base
 import Pirouette.Term.Syntax.Pretty
 import Pirouette.Term.Syntax.Subst
 import Pirouette.Term.Syntax.SystemF
+
+import Pirouette.Transformations.Utils
 
 -- * Monomorphization
 --
@@ -89,22 +88,18 @@ import Pirouette.Term.Syntax.SystemF
 -- And, since SMTLIB does support polymorphism, we can leave type variables not occuring in a higher-order context,
 -- potentially reducing the number of extra specialized terms.
 
-traceDefsId :: (PrettyLang lang) => PrtUnorderedDefs lang -> PrtUnorderedDefs lang
-traceDefsId defs = renderSingleLineStr (pretty $ prtUODecls defs) `trace` defs
-
 monomorphize :: forall lang. (PrettyLang lang, Pretty (FunDef lang Name), LanguageDef lang)
              => PrtUnorderedDefs lang
              -> PrtUnorderedDefs lang
-monomorphize defs0 = traceDefsId (prune $ go mempty defs0)
+monomorphize defs0 = prune $ go mempty defs0
   where
-    hofDefsRoots = findHOFDefs $ prtUODecls defs0
+    hofDefsRoots = findPolyHOFDefs $ prtUODecls defs0
     hofDefs = hofsClosure (prtUODecls defs0) hofDefsRoots
 
     go :: S.Set (SpecRequest lang) -> PrtUnorderedDefs lang -> PrtUnorderedDefs lang
     go prevOrders defs
       | M.null newDefs && defs == defs' = defs'
-      | otherwise = -- renderSingleLineStr (pretty (head $ M.elems $ head $ executeSpecRequest <$> specOrders)) `trace`
-                    go (prevOrders <> S.fromList newOrders) $ addDecls newDefs defs'
+      | otherwise = go (prevOrders <> S.fromList newOrders) $ addDecls newDefs defs'
       where
         (defs', specOrders) = runWriter $ transformBiM (specFunApp hofDefs :: SpecFunApp lang) defs
                                       >>= transformBiM (specTyApp  hofDefs :: SpecTyApp  lang)
@@ -146,8 +141,8 @@ executeSpecRequest SpecRequest {origDef = HofDef{..}, ..} = M.fromList $
 specFunApp :: forall lang. (LanguageDef lang) => HOFDefs lang -> SpecFunApp lang
 specFunApp hofDefs (App (F (FreeName name)) args)
   | Just someDef <- name `M.lookup` hofDefs    -- TODO should just the nameString be compared?
-  , all isSpecArg $ take (hofPolyVarsCount someDef) tyArgs = do
-    let (specArgs, remainingArgs) = splitArgs (hofPolyVarsCount someDef) args
+  , all isSpecArg $ take hofPolyVarsCount tyArgs = do
+    let (specArgs, remainingArgs) = splitArgs hofPolyVarsCount args
         speccedName = genSpecName specArgs name
     tell $ pure $ SpecRequest someDef specArgs
     pure $ F (FreeName speccedName) `App` remainingArgs
@@ -157,17 +152,25 @@ specFunApp hofDefs (App (F (FreeName name)) args)
     splitArgs n (TyArg tyArg : args) = first (tyArg :) $ splitArgs (n - 1) args
     splitArgs n (arg : args) = second (arg :) $ splitArgs n args
     splitArgs _ [] = error "Less args than poly args count"
+    hofPolyVarsCount = 1 -- TODO don't hardcode 1
 specFunApp _ x = pure x
 
 specTyApp :: (LanguageDef lang) => HOFDefs lang -> SpecTyApp lang
 specTyApp hofDefs (TyApp (F (TyFree name)) tyArgs)
   | Just someDef <- name `M.lookup` hofDefs
-  , all isSpecArg $ take (hofPolyVarsCount someDef) tyArgs = do
-    let (specArgs, remainingArgs) = splitAt (hofPolyVarsCount someDef) tyArgs
+  , all isSpecArg $ take hofPolyVarsCount tyArgs = do
+    let (specArgs, remainingArgs) = splitAt hofPolyVarsCount tyArgs
         speccedName = genSpecName specArgs name
     tell $ pure $ SpecRequest someDef specArgs
     pure $ F (TyFree speccedName) `TyApp` remainingArgs
+  where
+    hofPolyVarsCount = 1 -- TODO don't hardcode 1
 specTyApp _ x = pure x
+-- TODO the above definition shouldn't have hofPolyVarsCount as the _count_,
+-- since the HOF-involved type args aren't necessarily at the front of the application.
+-- A smarter approach would be to keep a list of type variables need to be monomorphized,
+-- but this kludge together with hardcoding it to be `1` gets us far enough
+-- to work with SMT on some realistic examples.
 
 argsToStr :: (LanguageDef lang) => [PrtType lang] -> T.Text
 argsToStr = T.intercalate "@" . map f
@@ -186,92 +189,24 @@ isSpecArg arg = null bounds
     bounds :: [VarMeta Void Name (TypeBase lang Name)]
     bounds = filter (isJust . isBound) $ universeBi arg
 
--- Types can't depend on terms, so it's safe to move all type applications to the front.
--- Also, this function is obviously broken right now.
-reorderAppTys :: forall lang. LanguageDef lang
-              => PrtUnorderedDefs lang
-              -> PrtUnorderedDefs lang
-reorderAppTys = transformBi f . error "this is currently broken"
-  where
-    f :: TermMeta lang Void Name -> TermMeta lang Void Name
-    f (App vm args) = App vm (reorder args)
-    f t = t
-
-    -- TODO fix de Bruijn indices
-    reorder = uncurry (<>) . partition isTyArg
-
-data HofDefBody lang
-  = HDBType (TypeDef lang Name)
-  | HDBFun  (FunDef lang Name)
-  deriving (Show, Eq, Ord)
-
-data HofDef lang = HofDef
-  { hofDefName :: Name
-  , hofPolyVarsCount :: Int
-  , hofDefBody :: HofDefBody lang
-  }
-  deriving (Show, Eq, Ord)
--- TODO the above definition shouldn't have hofPolyVarsCount as the _count_,
--- since the HOF-involved type args aren't necessarily at the front of the application.
--- A smarter approach would be to keep a list of type variables need to be monomorphized,
--- but this kludge together with hardcoding it to be `1` gets us far enough
--- to work with SMT on some realistic examples.
-
-type HOFDefs lang = M.Map Name (HofDef lang)
-
--- Returns the definitions containing higher-order functions,
+-- Returns the definitions containing (polymorphic) higher-order functions,
 -- be it functions or types.
 --
 -- The returned Map maps any name that contains higher-order stuff.
 -- In particular, for a type it contains the type itself as well as
 -- the type's constructors and destructor.
-findHOFDefs :: LanguageDef lang => Decls lang Name -> HOFDefs lang
-findHOFDefs decls = M.fromList $ hofTypes <> hofFuns
-  where
-    hofTypes = mkTypeHofDefs declsPairs $ \_ typeDef -> (hasHOFuns . snd) `any` constructors typeDef
-    hofFuns = findPolyFuns (hasHOFuns . funTy) declsPairs
-    declsPairs = M.toList decls
-
-isPolyType :: AnnType ann ty -> Bool
-isPolyType TyAll {} = True
-isPolyType _ = False
-
-isPolyFun :: FunDef lang ann -> Bool
-isPolyFun = isPolyType . funTy
-
-findFuns :: LanguageDef lang
-         => (forall ann. Data ann => FunDef lang ann -> Bool)
-         -> [(Name, Definition lang Name)]
-         -> [(Name, HofDef lang)]
-findFuns pred declsPairs =
-  [ (name, HofDef name 1 $ HDBFun funDef)  -- TODO don't hardcode 1
-  | (name, DFunDef funDef) <- declsPairs
-  , pred funDef
-  ]
+findPolyHOFDefs :: LanguageDef lang => Decls lang Name -> HOFDefs lang
+findPolyHOFDefs = findHOFDefs (isPolyType . funTy) (const $ const True) . M.toList
 
 findPolyFuns :: LanguageDef lang
              => (forall ann. Data ann => FunDef lang ann -> Bool)
              -> [(Name, Definition lang Name)]
              -> [(Name, HofDef lang)]
-findPolyFuns pred = findFuns (\f -> isPolyFun f && pred f)
+findPolyFuns pred = findFuns (\f -> isPolyType (funTy f) && pred f)
 
-mkTypeHofDefs :: LanguageDef lang
-              => [(Name, Definition lang Name)]
-              -> (Name -> TypeDef lang Name -> Bool)
-              -> [(Name, HofDef lang)]
-mkTypeHofDefs declsPairs pred =
-  [ (name, HofDef tyName 1 $ HDBType typeDef)  -- TODO don't hardcode 1
-  | (tyName, DTypeDef typeDef) <- declsPairs
-  , pred tyName typeDef
-  , name <- tyName : destructor typeDef : (fst <$> constructors typeDef)
-  ]
-
-hasHOFuns :: (Data ann, Data ty) => AnnType ann ty -> Bool
-hasHOFuns ty = isHOFTy `any` [ f | f@TyFun {} <- universe ty ]
-
-isHOFTy :: AnnType ann ty -> Bool
-isHOFTy (TyFun TyFun {} _) = True
-isHOFTy _ = False
+isPolyType :: AnnType ann ty -> Bool
+isPolyType TyAll {} = True
+isPolyType _ = False
 
 
 hofsClosure :: forall lang. (LanguageDef lang, PrettyLang lang) => Decls lang Name -> HOFDefs lang -> HOFDefs lang
@@ -284,21 +219,9 @@ hofsClosure decls = go
       | otherwise = go hofs'
       where
         hofs' = hofs <> M.fromList (hofTypes' <> hofFuns')
-        hofTypes' = mkTypeHofDefs declsPairs $ \_ typeDef -> hasHofName typeDef
+        hofTypes' = findTypes declsPairs $ \_ typeDef -> hasHofName typeDef
         hofFuns' = findPolyFuns hasHofName declsPairs
 
         hasHofName :: (Data a) => a -> Bool
         hasHofName entity = not (null [ () | TyFree name   <- universeBi entity :: [TypeBase lang Name], name `M.member` hofs ])
                          || not (null [ () | FreeName name <- universeBi entity :: [TermBase lang Name], name `M.member` hofs ])
-
-
--- This really belongs to a Pretty module, but we need them here for nicer debugging anyway for now.
-instance (Pretty (BuiltinTypes lang), Pretty (FunDef lang Name)) => Pretty (HofDefBody lang) where
-  pretty (HDBType defn) = "<typ>" <+> pretty defn
-  pretty (HDBFun  defn) = "<fun>" <+> pretty defn
-
-instance (Pretty (BuiltinTypes lang), Pretty (FunDef lang Name)) => Pretty (HofDef lang) where
-  pretty = pretty . hofDefBody
-
-instance (Pretty (BuiltinTypes lang), Pretty (FunDef lang Name)) => Pretty (HOFDefs lang) where
-  pretty = align . vsep . map (\(n, d) -> pretty n <+> pretty d) . M.toList
---
+++ src/Pirouette/Transformations/Utils.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Pirouette.Transformations.Utils where
+
+import Data.Data
+import Data.Generics.Uniplate.Data
+import qualified Data.Map as M
+import Prettyprinter hiding (Pretty, pretty)
+
+import Debug.Trace
+
+import Pirouette.Monad
+import Pirouette.Term.Syntax
+import Pirouette.Term.Syntax.Base
+import Pirouette.Term.Syntax.SystemF
+
+traceDefsId :: (PrettyLang lang) => PrtUnorderedDefs lang -> PrtUnorderedDefs lang
+traceDefsId defs = renderSingleLineStr (pretty $ prtUODecls defs) `trace` defs
+
+data HofDefBody lang
+  = HDBType (TypeDef lang Name)
+  | HDBFun  (FunDef lang Name)
+  deriving (Show, Eq, Ord)
+
+data HofDef lang = HofDef
+  { hofDefName :: Name
+  , hofDefBody :: HofDefBody lang
+  }
+  deriving (Show, Eq, Ord)
+
+type HOFDefs lang = M.Map Name (HofDef lang)
+
+findFuns :: LanguageDef lang
+         => (forall ann. Data ann => FunDef lang ann -> Bool)
+         -> [(Name, Definition lang Name)]
+         -> [(Name, HofDef lang)]
+findFuns pred declsPairs =
+  [ (name, HofDef name $ HDBFun funDef)
+  | (name, DFunDef funDef) <- declsPairs
+  , pred funDef
+  ]
+
+findTypes :: LanguageDef lang
+          => [(Name, Definition lang Name)]
+          -> (Name -> TypeDef lang Name -> Bool)
+          -> [(Name, HofDef lang)]
+findTypes declsPairs pred =
+  [ (name, HofDef tyName $ HDBType typeDef)
+  | (tyName, DTypeDef typeDef) <- declsPairs
+  , pred tyName typeDef
+  , name <- tyName : destructor typeDef : (fst <$> constructors typeDef)
+  ]
+
+findHOFDefs :: forall lang. LanguageDef lang
+            => (forall ann. Data ann => FunDef lang ann -> Bool)
+            -> (Name -> TypeDef lang Name -> Bool)
+            -> [(Name, Definition lang Name)]
+            -> HOFDefs lang
+findHOFDefs funPred tyPred declsPairs = M.fromList $ findFuns funPred' declsPairs <> findTypes declsPairs tyPred'
+  where
+    funPred' :: forall ann. Data ann => FunDef lang ann -> Bool
+    funPred' fun = funPred fun && hasHOFuns (funTy fun)
+    tyPred' name typeDef = tyPred name typeDef && (hasHOFuns . snd) `any` constructors typeDef
+
+hasHOFuns :: (Data ann, Data ty) => AnnType ann ty -> Bool
+hasHOFuns ty = isHOFTy `any` [ f | f@TyFun {} <- universe ty ]
+
+isHOFTy :: AnnType ann ty -> Bool
+isHOFTy (TyFun TyFun {} _) = True
+isHOFTy _ = False
+
+-- This really belongs to a Pretty module, but we need them here for nicer debugging anyway for now.
+instance (Pretty (BuiltinTypes lang), Pretty (FunDef lang Name)) => Pretty (HofDefBody lang) where
+  pretty (HDBType defn) = "<typ>" <+> pretty defn
+  pretty (HDBFun  defn) = "<fun>" <+> pretty defn
+
+instance (Pretty (BuiltinTypes lang), Pretty (FunDef lang Name)) => Pretty (HofDef lang) where
+  pretty = pretty . hofDefBody
+
+instance (Pretty (BuiltinTypes lang), Pretty (FunDef lang Name)) => Pretty (HOFDefs lang) where
+  pretty = align . vsep . map (\(n, d) -> pretty n <+> pretty d) . M.toList
