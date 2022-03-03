35b5431da68f8d169fed0ca377d30a5227d80e16
0xd34df00d <0xd34df00d@gmail.com>
2022-03-03 14:22:25 -0600

    No need to apply, just replace the types in the arguments
    We'll also need to replace the applications with calls to Apply, but
    I'll do that later.

--- src/Pirouette/Transformations/Defunctionalization.hs
+++ src/Pirouette/Transformations/Defunctionalization.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Pirouette.Transformations.Defunctionalization(defunctionalize) where
 
@@ -55,54 +56,53 @@ defunDefs defs = defs { prtUODecls = defunDef <$> prtUODecls defs }
                                     . trace ("got: " <> renderSingleLineStr (pretty ty'))
                                     . trace ("var: " <> show hofs)
                                     . trace ("body was: " <> renderSingleLineStr (pretty funBody))
-                                    . trace ("body got: " <> renderSingleLineStr (pretty $ rewriteHofBody hofs funBody))
+                                    . trace ("body got: " <> renderSingleLineStr (pretty $ rewriteHofBodyArgTypes hofs funBody))
                                  else id)
                               FunDef{..}
 
-data SubstIndex = SubstIndex
-  deriving (Show)
-
 data PosApply lang = PosApply
-  { applyArg :: PrtArg lang
-  , synthType :: Either SubstIndex (B.Type lang Name)
+  { synthType :: B.Type lang Name
+  , applyFunName :: Name
   } deriving (Show)
 
 -- Changes the type of the form @Ty1 -> (Ty2 -> Ty3) -> Ty4@ to @Ty1 -> Closure[Ty2->Ty3] -> Ty4@
 -- where the @Closure[Ty2->Ty3]@ is the ADT with the labels and environments for the funargs of type @Ty2 -> Ty3@.
 rewriteHofType :: forall lang. (PrettyLang lang, LanguageDef lang)
                => B.Type lang Name
-               -> (B.Type lang Name, [PosApply lang])
+               -> (B.Type lang Name, [Maybe (PosApply lang)])
 rewriteHofType = go 0
   where
-    go :: Integer -> B.Type lang Name -> (B.Type lang Name, [PosApply lang])
-    go pos (dom `TyFun` cod) = (dom' `TyFun` cod', PosApply{ synthType = Right dom', .. } : applies)
+    go :: Integer -> B.Type lang Name -> (B.Type lang Name, [Maybe (PosApply lang)])
+    go pos (dom `TyFun` cod) = (dom' `TyFun` cod', posApply : applies)
       where
         (cod', applies) = go (pos + 1) cod
         thisIdx = App (B (Ann "") pos) []
-        (dom', applyArg) =
+        (dom', posApply) =
           case dom of
                TyFun{} -> let tyStr = funTyStr dom
                               closureTypeName = [i|Closure[#{tyStr}]|]
                               applyFunName = [i|Apply[#{tyStr}]|]
-                           in (TyApp (F $ TyFree closureTypeName) [], Arg $ App (F (FreeName applyFunName)) [Arg thisIdx])
-               _ -> (dom, Arg thisIdx)
+                              synthType = TyApp (F $ TyFree closureTypeName) []
+                           in (synthType, Just PosApply{..})
+               _ -> (dom, Nothing)
     go _    ty@TyApp{} = (ty, []) -- this doesn't defun things like `List (foo -> bar)`, which is fine for now
-    go pos (TyAll ann k ty) = TyAll ann k *** (PosApply{..} :) $ go (pos + 1) ty
-      where
-        synthType = Left SubstIndex
-        applyArg = TyArg $ TyApp (B (Ann "") pos) []
+    go pos (TyAll ann k ty) = TyAll ann k *** (Nothing :) $ go (pos + 1) ty
     go pos (TyLam ann k ty) = error "unexpected arg type" -- TODO mention the type
 
 -- Assumes the body is normalized enough so that all the binders are at the front.
-rewriteHofBody :: forall lang. (PrettyLang lang, LanguageDef lang)
-               => [PosApply lang]
-               -> B.Term lang Name
-               -> B.Term lang Name
-rewriteHofBody [] body = body
-rewriteHofBody applies body =
-  body `appN` [ applyArg
-              | (PosApply{..}, idx) <- zip applies [0..]
-              ]
+rewriteHofBodyArgTypes :: forall lang. (PrettyLang lang, LanguageDef lang)
+                       => [Maybe (PosApply lang)]
+                       -> B.Term lang Name
+                       -> B.Term lang Name
+rewriteHofBodyArgTypes [] = id
+rewriteHofBodyArgTypes (Nothing : rest) =
+  \case Lam ann ty body -> Lam ann ty $ rewriteHofBodyArgTypes rest body
+        Abs ann kd body -> Abs ann kd $ rewriteHofBodyArgTypes rest body
+        App{} -> error "unexpected App, arguments not exhausted yet"
+rewriteHofBodyArgTypes (Just PosApply{..} : rest) =
+  \case Lam ann ty body -> Lam ann synthType $ rewriteHofBodyArgTypes rest body
+        Abs ann kd body -> error "unexpected Abs where a type should've been replaced"
+        App{} -> error "unexpected App, arguments not exhausted yet"
 
 funTyStr :: (PrettyLang lang, LanguageDef lang) => B.Type lang Name -> T.Text
 funTyStr (dom `TyFun` cod) = funTyStr dom <> " => " <> funTyStr cod
