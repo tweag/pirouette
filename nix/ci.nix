dd486677451e58a8de382c5e18a745084493c33c
Alejandro Serrano <trupill@gmail.com>
2022-04-08 11:54:10 +0200

    Move CVC4 to common 'packages.nix'

--- nix/ci.nix
+++ nix/ci.nix
@@ -3,9 +3,6 @@
 { pkgs ? import (import ./sources.nix {}).nixpkgs {} }:
 let
   ourpkgs = import ./packages.nix {};
-  runtime-deps = [
-      ourpkgs.nixPkgsProxy.cvc4
-    ];
 in pkgs.mkShell {
     buildInputs = ourpkgs.build-deps ++ runtime-deps;
 }
