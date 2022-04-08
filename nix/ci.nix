550d40f3e30463cad9c6596194b9b2c46553054d
Alejandro Serrano <trupill@gmail.com>
2022-04-08 11:29:37 +0200

    Add CVC4 to CI Nix

--- nix/ci.nix
+++ nix/ci.nix
@@ -3,6 +3,9 @@
 { pkgs ? import (import ./sources.nix {}).nixpkgs {} }:
 let
   ourpkgs = import ./packages.nix {};
+  runtime-deps = [
+      ourpkgs.nixPkgsProxy.cvc4
+    ];
 in pkgs.mkShell {
-    buildInputs = ourpkgs.build-deps;
+    buildInputs = ourpkgs.build-deps ++ runtime-deps;
 }
