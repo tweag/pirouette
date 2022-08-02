ff0a3fb6c15c572d712823655c4185df27b837bf
Florent C <florentc@users.noreply.github.com>
2022-08-02 14:26:21 +0200

    Update src/Language/Pirouette/QuasiQuoter/Syntax.hs
    Co-authored-by: Victor Cacciari Miraldo <VictorCMiraldo@users.noreply.github.com>

--- src/Language/Pirouette/QuasiQuoter/Syntax.hs
+++ src/Language/Pirouette/QuasiQuoter/Syntax.hs
@@ -261,10 +261,10 @@ parseKind =
 
 -- | Parse a type. The following features are supported:
 --
--- - Application: `Either a b`
--- - Function arrow: `b -> (a -> b) -> c`
--- - Quantification: `forall (a : * -> *) (b : *) . `
--- - Type lambdas: `\(a : * -> *) (b : *) . `
+-- - Application: @Either a b@
+-- - Function arrow: @b -> (a -> b) -> c@
+-- - Quantification: @forall (a : * -> *) (b : *) . @
+-- - Type lambdas: @\(a : * -> *) (b : *) . @
 parseType :: forall lang. (LanguageParser lang) => Parser (Ty lang)
 parseType = P.label "Type" $ makeExprParser pAtom [[InfixL pApp], [InfixR pFun]]
   where
