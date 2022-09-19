20e38fc0032538ec98faa9ebd0bd2be1da081476
Quentin Aristote <quentin@aristote.fr>
2022-09-19 16:28:18 +0200

    benchmark: additional warmups and no shell

--- stamps/time.sh
+++ stamps/time.sh
@@ -8,7 +8,7 @@ rm results.md
 for stamp in $(ls *.smt)
 do
     echo "# $stamp" >> results.md
-    hyperfine --runs 250 --warmup 1 "z3 $stamp > /dev/null" "./with-shell-cmd-from-haskell.bin $stamp > /dev/null" --export-markdown tmp.md
+    hyperfine --runs 250 --warmup 5 -shell=none "z3 $stamp > /dev/null" "./with-shell-cmd-from-haskell.bin $stamp > /dev/null" --export-markdown tmp.md
     cat tmp.md >> results.md
     echo >> results.md
 done
