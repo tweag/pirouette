52fd8bc5049568595d7c0b6cd26dff6b8e532622
Quentin Aristote <quentin@aristote.fr>
2022-09-19 16:28:46 +0200

    benchmark: haskell: send commands one by one

--- stamps/with-shell-cmd-from-haskell.hs
+++ stamps/with-shell-cmd-from-haskell.hs
@@ -2,11 +2,28 @@ import System.Environment
 import System.IO
 import System.Process.Typed
 
-run :: String -> ProcessConfig () () ()
-run file = shell ("z3 " ++ file)
+config :: ProcessConfig Handle () ()
+config = setStdin createPipe $ shell "z3 -in"
+
+send :: Handle -> String -> IO ()
+send h cmd = do
+  hPutStrLn h cmd
+  hFlush h
+
+sendLines :: Handle -> [String] -> IO ()
+sendLines h [] = hClose h
+sendLines h (l : rest) = do
+  send h l
+  sendLines h rest
+
+transmit :: String -> Handle -> IO ()
+transmit file h = do
+  lines <$> readFile file >>= sendLines h
 
 main = do
   args <- getArgs
   case args of
-    [file] -> withProcessWait (run file) checkExitCode
+    [file] -> do
+      p <- startProcess config
+      transmit file (getStdin p)
     _ -> putStrLn "usage: ./with-shell-cmd-from-haskell <file>"
