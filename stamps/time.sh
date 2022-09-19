86b4a081107926eb382dc59485a87da5d7769f1d
Quentin Aristote <quentin@aristote.fr>
2022-09-19 10:59:46 +0200

    improve benchmarking and its output

--- stamps/time.sh
+++ stamps/time.sh
@@ -1,14 +1,15 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p z3
 
-rm results
+RUNS=250
+
+
+rm results.md
 for stamp in $(ls *.smt)
 do
-    echo "-----------------------------------" >> results
-    echo $stamp >> results
-    (time z3 $stamp > /dev/null) &>> results
-    echo "when run directly from a shell" >> results
-    (time ./with-shell-cmd-from-haskell $stamp > /dev/null) &>> results
-    echo "when launched from a haskell binary" >> results
-    echo >> results
+    echo "# $stamp" >> results.md
+    hyperfine --runs 250 --warmup 1 "z3 $stamp > /dev/null" "./with-shell-cmd-from-haskell $stamp > /dev/null" --export-markdown tmp.md
+    cat tmp.md >> results.md
+    echo >> results.md
 done
+rm tmp.md
