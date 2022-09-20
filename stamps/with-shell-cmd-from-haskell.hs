6ebedf613230b9f2e919e6b7e29bb19942680465
Quentin Aristote <quentin@aristote.fr>
2022-09-20 10:39:59 +0200

    wait for output before sending next command

--- stamps/with-shell-cmd-from-haskell.hs
+++ stamps/with-shell-cmd-from-haskell.hs
@@ -2,28 +2,33 @@ import System.Environment
 import System.IO
 import System.Process.Typed
 
-config :: ProcessConfig Handle () ()
-config = setStdin createPipe $ shell "z3 -in"
-
-send :: Handle -> String -> IO ()
-send h cmd = do
-  hPutStrLn h cmd
-  hFlush h
-
-sendLines :: Handle -> [String] -> IO ()
-sendLines h [] = hClose h
-sendLines h (l : rest) = do
-  send h l
-  sendLines h rest
-
-transmit :: String -> Handle -> IO ()
-transmit file h = do
-  lines <$> readFile file >>= sendLines h
+config :: ProcessConfig Handle Handle ()
+config = setStdin createPipe $ setStdout createPipe $ shell "z3 -in"
 
 main = do
   args <- getArgs
   case args of
     [file] -> do
       p <- startProcess config
-      transmit file (getStdin p)
+      transmit file (getStdin p) (getStdout p)
     _ -> putStrLn "usage: ./with-shell-cmd-from-haskell <file>"
+
+transmit :: String -> Handle -> Handle -> IO ()
+transmit file hIn hOut = do
+  lines <$> readFile file >>= sendLines hIn hOut
+
+sendLines :: Handle -> Handle -> [String] -> IO ()
+sendLines hIn hOut [] = hClose hIn
+sendLines hIn hOut (l : rest) = do
+  send hIn l >> recv hOut
+  sendLines hIn hOut rest
+
+send :: Handle -> String -> IO ()
+send h cmd = do
+  hPutStrLn h cmd
+  hFlush h
+
+recv :: Handle -> IO ()
+recv h = do
+  resp <- hGetLine h
+  putStrLn resp
