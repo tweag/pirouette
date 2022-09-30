cc2ab3b68d88f8fddaf53712f3a63dcf3779db0c
Quentin Aristote <quentin@aristote.fr>
2022-09-30 15:18:21 +0200

    measure solver initialization

--- src/PureSMT/Process.hs
+++ src/PureSMT/Process.hs
@@ -28,7 +28,7 @@ launchSolverWithFinalizer ::
   -- | Whether or not to debug the interaction
   Bool ->
   IO Solver
-launchSolverWithFinalizer cmd dbg = do
+launchSolverWithFinalizer cmd dbg = TimeStats.measureM "launchSolver" $ do
   solverConfig <- mkConfig
   solverState <- mkContext solverConfig
   let s =
