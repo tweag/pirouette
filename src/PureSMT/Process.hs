2f235bae0c1db649132549585d483f85b1ad0765
Quentin Aristote <quentin@aristote.fr>
2022-10-06 10:38:04 +0200

    benchmark serializing and solver separately

--- src/PureSMT/Process.hs
+++ src/PureSMT/Process.hs
@@ -46,7 +46,7 @@ launchSolverWithFinalizer cmd dbg = TimeStats.measureM "launchSolver" $ do
 
 command :: Solver -> SExpr -> IO SExpr
 command solver cmd = TimeStats.measureM "command" $ do
-  let !cmdTxt = TimeStats.measurePure "showsSExpr" $ showsSExpr cmd ""
+  let !cmdTxt = TimeStats.measurePure "showsSExpr" $ force $ showsSExpr cmd ""
   resp <- TimeStats.measureM "Z3" $ evalSMTLib2String (state solver) cmdTxt
   case TimeStats.measurePure "readSExpr" $ force $ readSExpr resp of
     Nothing -> do
