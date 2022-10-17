11059cbbfe3ac9c0d7675e334b516cf06b36dfad
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2022-10-17 17:24:26 +0200

    Test commutativity of insertions and unions

--- tests/unit/UnionFindSpec.hs
+++ tests/unit/UnionFindSpec.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module UnionFindSpec (tests) where
 
+import Data.Bifunctor (first)
 import Data.Function ((&))
+import Data.List (partition, sort)
+import qualified Data.List.NonEmpty as NE
 import qualified Test.QuickCheck as QC
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -38,6 +42,25 @@ basicSmoke =
       testCase "Lookup 9 in uf1" $ snd (UF.lookup 9 uf1) @?= Nothing
     ]
 
+data Action key value
+  = Insert key value
+  | Union key key
+  deriving (Show)
+
+instance (QC.Arbitrary key, QC.Arbitrary value) => QC.Arbitrary (Action key value) where
+  arbitrary =
+    QC.arbitrary >>= \case
+      True -> Insert <$> QC.arbitrary <*> QC.arbitrary
+      False -> Union <$> QC.arbitrary <*> QC.arbitrary
+
+isInsert :: Action key value -> Bool
+isInsert (Insert _ _) = True
+isInsert _ = False
+
+applyAction :: (Ord key, Num value) => Action key value -> WithUnionFind key value ()
+applyAction (Insert k v) = insert (+) k v
+applyAction (Union k1 k2) = union (+) k1 k2
+
 props :: TestTree
 props =
   testGroup
@@ -45,7 +68,22 @@ props =
     [ testProperty "trivially inserting and finding the value succeeds" $
         \(k :: Int) (v :: Int) -> snd (UFI.lookup k $ UFI.trivialInsert k v UFI.empty) == Just v,
       testProperty "inserting combines" $
-        \(k :: Int) (v1 :: Int) v2 -> snd (UFI.lookup k $ UFI.insert (+) k v2 $ UFI.insert (+) k v1 UFI.empty) == Just (v1 + v2)
+        \(k :: Int) (v1 :: Int) v2 -> snd (UFI.lookup k $ UFI.insert (+) k v2 $ UFI.insert (+) k v1 UFI.empty) == Just (v1 + v2),
+      testProperty "reordering actions works" $
+        \(actions :: [Action Int Int]) ->
+          let (inserts, unions) = partition isInsert actions
+              insertsFirst = inserts ++ unions
+              unionsFirst = unions ++ inserts
+              -- Run the same actions in different orders
+              result1 = snd $ runWithUnionFind $ mapM applyAction actions
+              result2 = snd $ runWithUnionFind $ mapM applyAction insertsFirst
+              result3 = snd $ runWithUnionFind $ mapM applyAction unionsFirst
+              -- Normalise outputs
+              sorted1 = sort $ map (first (sort . NE.toList)) $ snd $ UFI.toList result1
+              sorted2 = sort $ map (first (sort . NE.toList)) $ snd $ UFI.toList result2
+              sorted3 = sort $ map (first (sort . NE.toList)) $ snd $ UFI.toList result3
+           in sorted1 QC.=== sorted2
+                QC..&&. sorted1 QC.=== sorted3
     ]
 
 tests :: [TestTree]
